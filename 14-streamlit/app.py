66eeb0bd329672e727d8f20e075ac6e24508a1ea
sivahere4cloud <sivaganeshcloud@gmail.com>
2026-07-13 00:08:07 +0100

    Basic streamlit practice

--- 14-streamlit/app.py
+++ 14-streamlit/app.py
@@ -1,1 +1,28 @@
-pip install 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#Title of the application
+st.title("Hello Streamlit!")
+
+#Display a simple text
+st.write("This is a simple Streamlit application.")
+
+# create a sample DataFrame
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+
+## Display the Dataframe
+st.write("Here is the dataframe")
+st.write(df)
+
+
+##create a line chart
+
+chart_data=pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b','c']
+)
+st.line_chart(chart_data)
